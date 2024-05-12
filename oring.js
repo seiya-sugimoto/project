b5e958e92d880ddd80586aa20d22f717421ebf86
seiya-sugimoto <seiyajac317@gmail.com>
2024-05-12 18:07:58 +0900

    Add files via upload

---
+++ oring.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetElement = document.querySelector(this.getAttribute('href'));
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
